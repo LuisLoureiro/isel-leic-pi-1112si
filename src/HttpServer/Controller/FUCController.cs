5f7d79b7563ec66733e4ba964c032789c9821101
Ricardo Andrade <randrade@alunos.isel.ipl.pt>
2011-10-21 21:26:47 +0100

    Created Proposal Controller to nest some methods that where previously in FUC Controller

--- src/HttpServer/Controller/FUCController.cs
+++ src/HttpServer/Controller/FUCController.cs
@@ -58,35 +58,5 @@ namespace HttpServer.Controller
         {
             return null;
         }
-
-        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props/{id}")]
-        public HttpResponse GetFucProposal(string acr, int id)
-        {
-            return null;
-        }
-
-        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props")]
-        public HttpResponse GetFucProposals(string acr)
-        {
-            return null;
-        }
-
-        [HttpCmd(HttpMethod.Post, "/fucs/{acr}/props/{id}/accept")]
-        public HttpResponse PostAcceptFucProposal(string acr, int id)
-        {
-            return null;
-        }
-
-        [HttpCmd(HttpMethod.Post, "/fucs/{acr}/props/{id}/cancel")]
-        public HttpResponse PostCancelFucProposal(string acr, int id)
-        {
-            return null;
-        }
-
-        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props/{id}/edit")]
-        public HttpResponse GetEditFucProposal(string acr, int id)
-        {
-            return null;
-        }
     }
 }
---
+++ src/HttpServer/Controller/ProposalController.cs
@@ -0,0 +1,38 @@
+﻿using PI.WebGarten;
+using PI.WebGarten.MethodBasedCommands;
+
+namespace HttpServer.Controller
+{
+    class ProposalController
+    {
+        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props/{id}")]
+        public HttpResponse GetFucProposal(string acr, int id)
+        {
+            return null;
+        }
+
+        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props")]
+        public HttpResponse GetFucProposals(string acr)
+        {
+            return null;
+        }
+
+        [HttpCmd(HttpMethod.Post, "/fucs/{acr}/props/{id}/accept")]
+        public HttpResponse PostAcceptFucProposal(string acr, int id)
+        {
+            return null;
+        }
+
+        [HttpCmd(HttpMethod.Post, "/fucs/{acr}/props/{id}/cancel")]
+        public HttpResponse PostCancelFucProposal(string acr, int id)
+        {
+            return null;
+        }
+
+        [HttpCmd(HttpMethod.Get, "/fucs/{acr}/props/{id}/edit")]
+        public HttpResponse GetEditFucProposal(string acr, int id)
+        {
+            return null;
+        }
+    }
+}
