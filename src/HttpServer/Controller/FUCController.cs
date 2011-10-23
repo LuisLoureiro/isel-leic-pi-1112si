3a4ceaba71256ef72e33da0b91b6c71ec9160596
Ricardo Andrade <randrade@alunos.isel.ipl.pt>
2011-10-23 19:57:15 +0100

    Created POST for /fucs/{acr}/edit URI Template

--- src/HttpServer/Controller/FUCController.cs
+++ src/HttpServer/Controller/FUCController.cs
@@ -1,11 +1,13 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Security.Principal;
 using HttpServer.Model.Entities;
 using HttpServer.Model.Repository;
 using HttpServer.Views;
 using PI.WebGarten;
 using PI.WebGarten.MethodBasedCommands;
-using System;
+using System.Linq;
 
 namespace HttpServer.Controller
 {
@@ -28,15 +30,16 @@ namespace HttpServer.Controller
         public HttpResponse GetFuc(string acr, IPrincipal principal)
         {
             CurricularUnit fuc;
-            
+
             try
             {
                 fuc = _repo.GetById(acr);
-            }catch(Exception)
+            }
+            catch (Exception)
             {
                 return new HttpResponse(HttpStatusCode.NotFound);
             }
-            
+
             return new HttpResponse(HttpStatusCode.OK, new FucsView(fuc, principal));
         }
 
@@ -55,11 +58,32 @@ namespace HttpServer.Controller
         }
 
         [HttpCmd(HttpMethod.Post, "/fucs/{acr}/edit")]
-        public HttpResponse PostEditFucForm(string acr)
+        public HttpResponse PostEditFucForm(string acr, IEnumerable<KeyValuePair<string, string>> content, IPrincipal principal)
         {
-            //TODO
+            float ects;
+            if (!float.TryParse(content.Where(p => p.Key.Equals("ects")).FirstOrDefault().Value, out ects))
+                throw new ArgumentException("Valor de ECTS inválido");
 
-            return null;
+            var uc = new CurricularUnit(content.Where(p => p.Key.Equals("name")).FirstOrDefault().Value,
+                                        content.Where(p => p.Key.Equals("acr")).FirstOrDefault().Value,
+                                        content.Where(p => p.Key.Equals("tipoObrig")).FirstOrDefault().Value.Equals("obrigatoria"),
+                                        TranslateSemester(content),
+                                        ects);
+            var prop = new Proposal(uc, principal.Identity.Name);
+            RepositoryLocator.Get<long, Proposal>().Insert(prop);
+
+            return new HttpResponse(HttpStatusCode.SeeOther).WithHeader("Location", ResolveUri.For(prop));
+        }
+
+        private static ushort TranslateSemester(IEnumerable<KeyValuePair<string, string>> content)
+        {
+            ushort val = 0x00;
+
+            for(int i = 1; i<=10; i++)
+                if (content.Select(k => k.Key.Equals("" + i)) != null)
+                    val = (ushort)(val | (0x01 << (i - 1)));
+
+            return val;
         }
     }
 }
