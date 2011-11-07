99c1479c18f25db1514bbf775fcc6590426cb6b5
Ricardo Andrade <randrade@alunos.isel.ipl.pt>
2011-11-07 21:54:11 +0000

    Created Controllers

---
+++ src/mvc/Controllers/FucController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class FucController : Controller
+    {
+        //
+        // GET: /Uc/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string acr)
+        {
+            //TODO
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            //TODO
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(FucModel model)
+        {
+            //TODO
+            var prop = new PropModel();
+            return RedirectToAction("Details", "Prop", prop.Id);
+        }
+
+        public ActionResult Edit(string acr)
+        {
+            //TODO
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FucModel model)
+        {
+            //TODO
+            var prop = new PropModel();
+            return RedirectToAction("Details", "Prop", prop.Id);
+        }
+    }
+}
---
+++ src/mvc/Controllers/PropController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers
+{
+    public class PropController : Controller
+    {
+        //
+        // GET: /Prop/
+
+        public ActionResult Index()
+        {
+            //TODO
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            //TODO - passar à vista um modelo
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //TODO
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PropModel model)
+        {
+            //TODO
+            var prop = new PropModel();
+            return RedirectToAction("Details", "Prop", prop.Id);
+        }
+
+        [HttpPost]
+        public ActionResult Accept(int id)
+        {
+            //TODO
+            return RedirectToAction("Index", "Prop");
+        }
+
+        [HttpPost]
+        public ActionResult Cancel(int id)
+        {
+            //TODO
+            return RedirectToAction("Index", "Prop");
+        }
+    }
+}
