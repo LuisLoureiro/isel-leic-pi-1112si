61d0b2dd604b8613daa587e46dbea551b885d60e
Ricardo Andrade <randrade@alunos.isel.ipl.pt>
2012-01-02 20:08:21 +0000

    Created classes to support pagination

---
+++ src/mvc/Models/TableViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace mvc.Models
+{
+    public class TableViewModel
+    {
+        public IEnumerable Items { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
