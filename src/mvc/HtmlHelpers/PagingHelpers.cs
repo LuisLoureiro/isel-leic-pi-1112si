7884547d8d33b3ab3e2ff87038a35dc8e64162db
Ricardo Andrade <randrade@alunos.isel.ipl.pt>
2012-01-17 19:55:57 +0000

    Removed pagination when all items are being displayed.

--- src/mvc/HtmlHelpers/PagingHelpers.cs
+++ src/mvc/HtmlHelpers/PagingHelpers.cs
@@ -11,49 +11,53 @@ namespace mvc.HtmlHelpers
         public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, int, string> pageUrl)
         {
             var ulInnerHtml = new StringBuilder();
-            var li = new TagBuilder("li");
-            li.AddCssClass("prev");
 
-            var a = new TagBuilder("a") { InnerHtml = "Anterior" };
-
-            if (pagingInfo.CurrentPage == 1)
-                li.AddCssClass("disabled");
-            else
-                a.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage - 1, pagingInfo.ItemsPerPage));
-
-            li.InnerHtml = a.ToString();
-
-            ulInnerHtml.Append(li.ToString());
-
-            int totalPages = pagingInfo.TotalPages;
-            for (int i = 1; i <= totalPages; i++)
+            if (pagingInfo.TotalPages > 1)
             {
-                a = new TagBuilder("a"){ InnerHtml = i.ToString(CultureInfo.InvariantCulture) };
+                var li = new TagBuilder("li");
+                li.AddCssClass("prev");
 
-                a.MergeAttribute("href", pageUrl(i, pagingInfo.ItemsPerPage));
-                
+                var a = new TagBuilder("a") {InnerHtml = "Anterior"};
 
-                li = new TagBuilder("li"){ InnerHtml = a.ToString() };
+                if (pagingInfo.CurrentPage == 1)
+                    li.AddCssClass("disabled");
+                else
+                    a.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage - 1, pagingInfo.ItemsPerPage));
 
-                if (i == pagingInfo.CurrentPage)
-                    li.AddCssClass("active");
+                li.InnerHtml = a.ToString();
+
+                ulInnerHtml.Append(li.ToString());
+
+                int totalPages = pagingInfo.TotalPages;
+                for (int i = 1; i <= totalPages; i++)
+                {
+                    a = new TagBuilder("a") {InnerHtml = i.ToString(CultureInfo.InvariantCulture)};
+
+                    a.MergeAttribute("href", pageUrl(i, pagingInfo.ItemsPerPage));
+
+
+                    li = new TagBuilder("li") {InnerHtml = a.ToString()};
+
+                    if (i == pagingInfo.CurrentPage)
+                        li.AddCssClass("active");
+
+                    ulInnerHtml.Append(li.ToString());
+                }
+
+                li = new TagBuilder("li");
+                li.AddCssClass("next");
+
+                a = new TagBuilder("a") {InnerHtml = "Próximo"};
+
+                if (pagingInfo.CurrentPage == pagingInfo.TotalPages)
+                    li.AddCssClass("disabled");
+                else
+                    a.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage + 1, pagingInfo.ItemsPerPage));
+
+                li.InnerHtml = a.ToString();
 
                 ulInnerHtml.Append(li.ToString());
             }
-
-            li = new TagBuilder("li");
-            li.AddCssClass("next");
-
-            a = new TagBuilder("a"){ InnerHtml = "Próximo" };
-
-            if (pagingInfo.CurrentPage == pagingInfo.TotalPages)
-                li.AddCssClass("disabled");
-            else
-                a.MergeAttribute("href", pageUrl(pagingInfo.CurrentPage + 1, pagingInfo.ItemsPerPage));
-
-            li.InnerHtml = a.ToString();
-
-            ulInnerHtml.Append(li.ToString());
 
             var ul = new TagBuilder("ul"){ InnerHtml = ulInnerHtml.ToString() };
 
